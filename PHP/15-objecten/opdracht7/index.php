824574572ae6c580aa89abd51aabc06f4cabaec3
KeoJ10 <keo@emerald-it.nl>
2025-02-21 10:07:47 +0100

    opdracht 7 objecten verbetered

--- PHP/15-objecten/opdracht7/index.php
+++ PHP/15-objecten/opdracht7/index.php
@@ -1,37 +1,7 @@
 <link href="css/stylesheet.css" rel="stylesheet">
 <?php 
-
-class User {
-	private string $name;
-	private string $password;
-	private string $email;
-	private int $age;
-	private bool $active = true;
-
-    public function getName(): string {
-		return $this->name;
-	}
-    public function getPassword(): string {
-		return $this->password;
-	}
-    public function getEmail(): string {
-		return $this->email;
-	}
-    public function getAge(): int {
-		return $this->age;
-	}
-    public function getActivity(): bool {
-		return $this->active;
-	}
-
-    public function __construct(?string $name = null, ?string $password = null, ?string $email = null, ?int $age = null , bool $active = true){
-        $this->name = $name;
-        $this->age = $age;
-        $this->email = $email;
-        $this->password = $password;
-        $this->active = $active;
-    }
-}
+require_once 'classes/users.php';
+use classes\users\User;
 
 class showData{
     public $showdata = '';
